6c0c33bfab54a5ebdde0104b12a6584a23dd9778
ravi <rg016541@gmail.com>
2025-02-26 00:11:18 +0530

    updated context

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,33 @@ import About from './components/about/About'
 import ContactUs from './components/contactus/ContactUs '
 import ViewProduct from './components/product/ViewProduct'
 import Cart from './components/cart/Cart';
+import AdminDashboard from './components/admin/AdminDashboard';
+import Login from './components/user/Login';
 
 export const store=createContext();
+//export const storeUser=createContext();
 
 
 
 function App() {
 
   const [cart,setCart]=useState([])
+  const [user,setUser]=useState([])
   return (
     <>
      <BrowserRouter>
-     <store.Provider value={[cart,setCart]} >
+     <store.Provider value={{ cart, setCart, user, setUser }} >
 
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/home' element={<Home/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<ContactUs/>} />
         <Route path='/cart' element={<Cart/>} />
         <Route path='/product/:id' element={<ViewProduct/>} />
+        <Route path='/admin' element={<AdminDashboard/>} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
      </store.Provider>
      </BrowserRouter>
